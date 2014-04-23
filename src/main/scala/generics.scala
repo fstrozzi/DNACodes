ebfbb5122eb1c92e3873d7d64194ad981e5989dc
Francesco Strozzi <francesco.strozzi@gmail.com>
2014-04-23 17:01:40 +0200

    OO version

---
+++ src/main/scala/generics.scala
@@ -0,0 +1,36 @@
+package DNABarcodes {
+
+	class Generics {
+		
+		val codex = Map[Char,Int]('A'-> 0, 'C' -> 1, 'G' -> 2, 'T' -> 3)
+		val codexReverse = Map[Int,Char](0 -> 'A', 1 -> 'C', 2 -> 'G', 3 -> 'T')
+
+		def generateParityBit(codeLength: Int, codeBits: Array[Int]) : Int = {
+			val bit = ((codeLength - codeBits.sum) % 4) % 4
+			if(bit < 0) {
+				return bit + codeLength
+			}
+			else {
+				return bit
+			}
+		}
+
+		def calculateParityBit(codeBits: Array[Int]) : Int = {
+			codeBits.sum % 4 
+		}
+
+		def quad2dna(hamming: Array[Int]) : Array[Char] = {
+			hamming.map(codexReverse(_))
+		}
+
+		def correctBase(errType: Int, baseAtError: Int) : Char  = {
+			val bit = (baseAtError - errType) % 4
+			if (bit < 0) {
+				return codexReverse(bit + 4)
+			}
+			else {
+				return codexReverse(bit)
+			}
+		}
+	}
+}
