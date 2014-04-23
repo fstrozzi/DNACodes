db116b67c301840c9335f33d5c25e7f5db9b4b92
Francesco Strozzi <francesco.strozzi@gmail.com>
2014-04-23 14:40:42 +0200

    working code for hamming74

--- src/main/scala/barcodes.scala
+++ src/main/scala/barcodes.scala
@@ -2,18 +2,18 @@ object DNABarcodes extends App {
 	val codex = Map[Char,Int]('A'-> 0, 'C' -> 1, 'G' -> 2, 'T' -> 3)
 	val codexReverse = Map[Int,Char](0 -> 'A', 1 -> 'C', 2 -> 'G', 3 -> 'T')
 
-	def hamming74(dna: Array[Char]) : Array[Int] = {
+	def hamming74(dna: Array[Char]) : Array[Char] = {
 		val data = dna.map(codex(_))
 		val codeLength = 4
 		val p1 = generateParityBit(codeLength, Array(data(0),data(1),data(3)))
 		val p2 = generateParityBit(codeLength, Array(data(0),data(2),data(3)))
 		val p3 = generateParityBit(codeLength, Array(data(1),data(2),data(3)))
 		val hamming = Array(p1,p2,data(0),p3,data(1),data(2),data(3))
-		hamming
+		quad2dna(hamming)
 	}
 	
 	def generateParityBit(codeLength: Int, codeBits: Array[Int]) : Int = {
-		val bit = ((codeLength - codeBits.sum) % codeLength) % codeLength
+		val bit = ((codeLength - codeBits.sum) % 4) % 4
 		if(bit < 0) {
 			return bit + codeLength
 		}
@@ -22,14 +22,65 @@ object DNABarcodes extends App {
 		}
 	}
 
-	def quad2dna(hamming: Array[Int]) : Array[Char] = {
-		val dna = hamming.map(codexReverse(_))
-		dna
+	def checkCode(barcode: Array[Char]) : Array[Char] = {
+		val quadCode = barcode.map(codex(_))
+		val p1 = calculateParityBit(Array(quadCode(0),quadCode(2),quadCode(4),quadCode(6)))
+		val p2 = calculateParityBit(Array(quadCode(1),quadCode(2),quadCode(5),quadCode(6)))
+		val p3 = calculateParityBit(Array(quadCode(3),quadCode(4),quadCode(5),quadCode(6)))
+		val parity = Array(p1,p2,p3)
+		println("Parity Code: "+parity.mkString("-"))
+		val errType = parity.max
+		if(errType > 0) {
+			val binCode = quad2bin(Array(p3,p2,p1))
+			val errorPosition = getErrorPosition(binCode)
+			println("ErrorPosition:"+errorPosition)
+			val trueBase = correctBase(errType,quadCode(errorPosition))
+			val correctedBarcode = barcode
+			correctedBarcode(errorPosition) = trueBase
+			return correctedBarcode
+		}
+		else {
+			return barcode
+		}
 	}
 
-	val barcode = Array('A','T','C','G')
-	val code = hamming74(barcode)
-	println(quad2dna(code).mkString)
+	def quad2bin(code: Array[Int]) : Array[Int] = {
+		code.map(el => {if(el > 0) 1 ; else 0})
+	}
 
+	def calculateParityBit(codeBits: Array[Int]) : Int = {
+		codeBits.sum % 4 
+	}
+
+	def quad2dna(hamming: Array[Int]) : Array[Char] = {
+		hamming.map(codexReverse(_))
+	}
+
+	def getErrorPosition(binCode: Array[Int]) : Int = {
+		Integer.parseInt(binCode.mkString,2).toInt - 1
+	}
+
+	def correctBase(errType: Int, baseAtError: Int) : Char  = {
+		val bit = (baseAtError - errType) % 4
+		if (bit < 0) {
+			return codexReverse(bit + 4)
+		}
+		else {
+			return codexReverse(bit)
+		}
+	}
+
+// TEST ZONE 
+
+	val barcode = Array('A','C','T','A')
+	val code = hamming74(barcode)
+	println("Original Code: " + code.mkString)
+	val wrongCode = code
+	wrongCode(4) = 'G'
+	println("Wrong Code: "+wrongCode.mkString)
+	val corrected = checkCode(wrongCode)
+	println("Corrected Code: "+ corrected.mkString)
+
+// END TEST ZONE
 
 }
